05736f7953809fedf1f2cef9c3d365d350734654
KonnorR <konnor0601@gmail.com>
2025-02-28 20:29:30 -0500

    2025.0.0-beta0.5.4

--- src/main/java/maniplib/Telemetry.java
+++ src/main/java/maniplib/Telemetry.java
@@ -4,23 +4,22 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Telemetry extends SubsystemBase {
 
-    public static ManipTelemetry manipVerbosity = ManipTelemetry.HIGH;
+  public static ManipTelemetry manipVerbosity = ManipTelemetry.HIGH;
 
-    public enum ManipTelemetry {
-        /*
-         * No telemetry data is sent to dashboard. Including sim.
-         */
-        NONE,
+  public enum ManipTelemetry {
+    /*
+     * No telemetry data is sent to dashboard. Including sim.
+     */
+    NONE,
 
-        /*
-         * Only basic telemetry data is sent to dashboard.
-         */
-        LOW,
+    /*
+     * Only basic telemetry data is sent to dashboard.
+     */
+    LOW,
 
-        /*
-         * All telemetry data is sent to dashboard.
-         */
-        HIGH
-    }
-
+    /*
+     * All telemetry data is sent to dashboard.
+     */
+    HIGH
+  }
 }
